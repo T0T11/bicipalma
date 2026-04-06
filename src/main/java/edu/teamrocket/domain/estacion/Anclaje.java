76ef2d7a1e26a070194e02d1f60a60e1826af3fd
T0T11 <tomas.orellano@gmail.com>
2026-04-06 16:39:13 +0200

    feat(Anclaje): creacion de la clase

--- src/main/java/edu/teamrocket/domain/estacion/Anclaje.java
+++ src/main/java/edu/teamrocket/domain/estacion/Anclaje.java
@@ -1,4 +1,43 @@
 package edu.teamrocket.domain.estacion;
 
+import edu.teamrocket.domain.bicicleta.Movil;
+
 public class Anclaje {
+    private boolean ocupado;
+    private Movil bici;
+
+
+    public Anclaje(){
+        this.ocupado = false;
+        this.bici = null;
+    }
+
+    public boolean isOcupado() {
+        return this.ocupado;
+    }
+
+    public Movil getBici() {
+        return  this.bici ;
+    }
+
+    public void   anclarBici(Movil bici){
+        this.bici = bici;
+        this.ocupado = true;
+
+    }
+
+    public void liberarBici(){
+        this.bici = null;
+        this.ocupado = false;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Anclaje{" +
+                "ocupado=" + ocupado +
+                ", bici=" + bici +
+                '}';
+    }
+
 }
