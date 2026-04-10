fde69b30d178f9140094e4cf79c0954c43bcf030
T0T11 <tomas.orellano@gmail.com>
2026-04-10 14:17:57 +0200

    chore(BiciPalma): borrado del main

---
+++ src/main/java/edu/teamrocket/domain/Bicipalma/BiciPalma.java
@@ -0,0 +1,11 @@
+package edu.teamrocket.domain.Bicipalma;
+
+import edu.teamrocket.domain.bicicleta.Bicicleta;
+import edu.teamrocket.domain.tarjetausuario.TarjetaUsuario;
+import edu.teamrocket.domain.estacion.Estacion;
+
+public class BiciPalma {
+
+
+}
+
